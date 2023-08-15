f317e3f667ee934ed201506e52c7f916f35ec7df
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-15 23:09:08 +0300

    feat: about page

---
+++ pages/company.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'home-layout',
+})
+
+useHead({
+  title: 'About',
+})
+</script>
+
+<template>
+  <section class="hero">
+    <VSheet class="h-screen">
+      <h1>THIS IS THE ABOUT PAGE</h1>
+    </VSheet>
+  </section>
+</template>
