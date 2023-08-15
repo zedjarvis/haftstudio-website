a2c9e9b3207f8a344b40d996834dd06760892743
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-15 23:08:45 +0300

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: true,
+  css: ['/assets/styles/main.scss'],
   modules: [
-    'vuetify-nuxt-module',
     '@vite-pwa/nuxt',
+    'vuetify-nuxt-module',
     '@unocss/nuxt',
     'nuxt-seo-experiments',
     '@nuxtjs/robots',
     'nuxt-simple-sitemap',
     'nuxt-og-image',
-    'nuxt-gtag',
+    // 'nuxt-gtag',
     'nuxt-vitest',
-    'nuxt-content-assets',
-    '@nuxt/content',
+    // '@nuxt/content',
+    // 'nuxt-content-assets',
     '@vueuse/nuxt',
     'nuxt-typed-router',
     '@pinia/nuxt',
-    '@sidebase/nuxt-pdf',
+    // '@sidebase/nuxt-pdf',
     '@nuxtjs/i18n',
-    '@tresjs/nuxt',
-    'nuxt3-leaflet',
+    // '@tresjs/nuxt',
+    // 'nuxt3-leaflet',
     // '@productdevbook/chatwoot',
-    '@nuxtjs/partytown',
+    // '@nuxtjs/partytown',
   ],
+  pinia: {
+    autoImports: [
+      'defineStore', 'storeToRefs',
+    ]
+  },
   vuetify: {
     moduleOptions: {
       styles: "sass",
     },
+    vuetifyOptions: {
+      theme: {
+        themes: {
+          light: {
+            dark: false,
+            colors: {
+              primary: '#8B4513',
+              secondary: '#444',
+              background: 'rgb(229, 231, 235)',
+              surface: 'rgb(218, 217, 213)'
+            }
+          }
+        }
+      },
+      defaults: {
+        VBtn: {
+          color: 'secondary',
+          rounded: 0,
+        }
+      }
+    }
   }
 })
