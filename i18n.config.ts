b4149b7ee8e7e48ebe4fd29249859d92784e247d
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-15 23:09:42 +0300

    feat: change locale en|fr|sw|zh

---
+++ i18n.config.ts
@@ -0,0 +1,45 @@
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: 'en',
+  messages: {
+    en: {
+      products: 'products',
+      company: 'company',
+      designers: 'designers',
+      work: 'work with us',
+      contact: 'contact',
+      aboutUs: 'about us',
+      homeDesc: `Established in 2011, Haft
+              Studio's visionary pursuit is enriched by its use of unique materials like matope-crete, timber, stone, and
+              papyrus reeds.This creative force harmoniously melds an African aesthetic into urban landscapes, setting
+              new benchmarks in creativity, authenticity, and reliability.`
+    },
+    sw: {
+      products: 'bidhaa',
+      company: 'kampuni',
+      designers: 'wabunifu',
+      work: 'fanya kazi nasi',
+      contact: 'mawasiliano',
+      aboutUs: 'kuhusu sisi',
+      homeDesc: `Ilianzishwa mwaka wa 2011, harakati ya maono ya Haft Studio inaboreshwa na matumizi yake ya vifaa vya kipekee kama vile matope-kreti, mbao, mawe, na matete ya mafunjo. Nguvu hii ya ubunifu inaunganisha kwa usawa urembo wa Kiafrika katika mandhari ya miji, kuweka vigezo vipya katika ubunifu, uhalisi, na kutegemewa.`
+    },
+    fr: {
+      products: 'des produits',
+      company: 'entreprise',
+      designers: 'concepteurs',
+      work: 'collaborer',
+      contact: 'contact',
+      aboutUs: 'à propos de nous',
+      homeDesc: `Fondée en 2011, la poursuite visionnaire de Haft Studio est enrichie par son utilisation de matériaux uniques comme la matope-crete, le bois, la pierre et les roseaux de papyrus. Cette force créative fusionne harmonieusement une esthétique africaine dans les paysages urbains, établissant de nouvelles références en matière de créativité, d'authenticité et de fiabilité.`
+    },
+    zh: {
+      products: '产品',
+      company: '公司',
+      designers: '设计师',
+      work: '合作',
+      contact: '接触',
+      aboutUs: '生物',
+      homeDesc: `Haft Studio 成立于 2011 年，通过使用马托普混凝土、木材、石头和纸莎草芦苇等独特材料，丰富了其远见卓识。这种创造力将非洲美学和谐地融入城市景观中，在创造力、真实性和可靠性。`
+    }
+  }
+}))
