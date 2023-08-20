8a8982688c60b4915f6773977a8fc00f4f47bd45
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-20 10:24:44 +0300

    feat: added designers index page

---
+++ pages/designers/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'home-layout',
+})
+
+useHead({
+  title: 'Our Designers',
+})
+</script>
+
+<template>
+  <h1>THIS IS THE DESIGNERS HOME PAGE</h1>
+</template>
