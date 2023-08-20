a658d95d3dbca43c8b6185ba6c0465143aa274cb
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-20 10:23:53 +0300

    feat: added blog index page

---
+++ pages/blog/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'home-layout',
+})
+
+useHead({
+  title: 'Our Stories',
+})
+</script>
+
+<template>
+  <h1>THIS IS THE BLOG HOME PAGE PAGE</h1>
+</template>
