6d31c7a8ffb7950867a74d33d3aa1c5aa2ea5703
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-20 10:25:06 +0300

    feat: added products index page

---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'home-layout',
+})
+
+useHead({
+  title: 'Our Products',
+})
+</script>
+
+<template>
+  <h1>THIS IS THE PRODUCT HOME PAGE</h1>
+</template>
