79a0d446eb6e914d71a69a927129a15dcb252700
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-20 10:25:29 +0300

    feat: added projects index page

---
+++ pages/projects/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'home-layout',
+})
+
+useHead({
+  title: 'Our Projects',
+})
+</script>
+
+<template>
+  <h1>THIS IS THE PROJECTS HOME PAGE</h1>
+</template>
