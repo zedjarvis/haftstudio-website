9d7119574d5999ff19baad75602eb36b9d859b6c
Cedrouseroll omondi <omondicedo@gmail.com>
2023-08-20 10:24:23 +0300

    feat: added contact page

---
+++ pages/contact.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'home-layout',
+})
+
+useHead({
+  title: 'Contact Us',
+})
+</script>
+
+<template>
+  <h1>THIS IS THE CONTACT PAGE</h1>
+</template>
