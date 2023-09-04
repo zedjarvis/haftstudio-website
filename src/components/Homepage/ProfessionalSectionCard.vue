d9dd1fda7105ae30b012c88b799dede1528c11a5
Cedrouseroll omondi <omondicedo@gmail.com>
2023-09-05 00:37:18 +0300

    Add Professional section card

---
+++ src/components/Homepage/ProfessionalSectionCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+// props
+const props = defineProps<{
+  image: string;
+  title: string;
+}>()
+</script>
+
+<template>
+  <VCard rounded="0" :elevation="0" class="pb-0">
+    <RouterLink to="/">
+      <VImg :src="props.image" :height="333" cover />
+    </RouterLink>
+    <RouterLink to="/">
+      <p class="pt-4 font-pacifico text-xl text-secondary mb-8" style="font-weight: 100;">{{ props.title }}</p>
+    </RouterLink>
+  </VCard>
+</template>
