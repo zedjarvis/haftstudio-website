cb7b25ea102f4ed85d7a7142ee06fd9aa9a86893
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-11-25 14:19:21 +0300

    refactor: move logo assets folder

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,6 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from '@/assets/images/logo.png';
+</script>
 <template>
   <RouterLink to="/">
-    <VImg src="/logo.png" alt="Haft Studio Logo" width="135" />
+    <VImg :src="Logo" alt="Haft Studio Logo" width="135" />
   </RouterLink>
 </template>
