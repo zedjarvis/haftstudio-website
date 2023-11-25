29ae1db2eccb74e554a00e6951aca5f220faa7d7
Cedrouseroll Omondi <omondicedo@gmail.com>
2023-11-25 13:24:41 +0300

    perf: use logo png to prevent FOUT on initial page load

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts"></script>
 <template>
   <RouterLink to="/">
-    <div class="logo">HaftStudio<span style="color:#444;">.</span></div>
+    <VImg src="/logo.png" alt="Haft Studio Logo" width="135" />
   </RouterLink>
 </template>
-
-<style lang="scss" scoped>
-.logo {
-  font-family: Pacifico !important;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 2.2rem;
-  width: auto;
-  color: #8B4513;
-}
-</style>
